e20eda408892c35a0b8d19f44858a5fdcf4408ff
Aravind Venkateswaran <z5208102@ad.unsw.edu.au>
2020-07-09 15:57:43 +1000

    paragraph to words function done, only neighboring words for this functionality

--- Scriptaswords.js
+++ Scriptaswords.js
@@ -1,15 +1,41 @@
- var string = 'get all combinations'    
- var everyComb = [];
- var x;
+
+function str2words(string){
+  var everyComb = [];
+  var x;
     var sArray = string.split(' ');
     var n = sArray .length;
     for (var i = 0; i < n; i++) {
       for (var j = 0; j <= i; j++) {
-        everyComb.push(sArray.slice(j, n - i + j).join(' '));
+        var temp = sArray.slice(j, n - i + j).join(' ');
+        if(temp.split(' ').length > 1 && temp.split(' ').length < 4){
+          everyComb.push(trim(temp));
+        }
       }
     }
-    
-    for (x of everyComb) {
-    	console.log(x)
-    }
+    return everyComb;
+}
 
+function revolt(para){
+  var FinalArray = [];
+  var strings = [];
+  var words = [];
+  strings = para.match(/\(?[^\.\?\!]+[\.!\?]\)?/g);
+  for (var iter of strings){
+      words = str2words(iter);
+      for(var word of words){
+        FinalArray.push(word);
+      }
+  }
+  return FinalArray;
+}
+
+function trim(str){
+  if(!str) return str;
+  var result = str.replace(/^\s+/g, '');
+  return  result.replace(/\s+$/g, '');
+}
+
+arrofs = revolt("I have an issue where I'm connected to mobile data (slow connection) and also on audio-only mode. But, my offline playlist (downloaded) is buffering which is absurd as I don't expect it to stream all the time from your servers.");
+for(var y of arrofs){
+    console.log(y);
+}
